5f301155444c07ed2c91fb85d2204d320d4f5832
Justin Hu <justin.hu@rwwa.com.au>
2024-09-07 18:26:04 +1000

    created first state

--- src/components/CourseCard/CourseCard.js
+++ src/components/CourseCard/CourseCard.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CourseCard.css'; // Importing the external CSS file
 
 // Course Card Component
 function CourseCard(props) {
+  const [isEnrolled, setIsEnrolled] = useState(false);
+
+  function handleEnroll() {
+    setIsEnrolled(true);
+  }
+
   return (
     <div className="course-card">
       <h2>{props.title}</h2>
       <p>{props.description}</p>
       <p>Number of Lessons: {props.lessons}</p>
-      <button>Enroll Now</button>
+      <button onClick={handleEnroll}>
+        {isEnrolled ? 'Enrolled!' : 'Enroll Now'}
+      </button>
     </div>
   );
 }
